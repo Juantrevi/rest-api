0dc31fef7ae5923dbf4ee1617a04716e8757f10f
Juantrevi <juantrevi70@gmail.com>
2023-09-23 19:36:05 +0100

    JSON-Response 1.0.2 (Config profile)

---
+++ src/main/java/com/springf/restapi/CurrencyServiceConfiguration.java
@@ -0,0 +1,50 @@
+package com.springf.restapi;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@EnableConfigurationProperties()
+@ConfigurationProperties(prefix = "currency-service")
+@Component
+public class CurrencyServiceConfiguration {
+
+    private String url;
+    private String username;
+    private String key;
+
+    public CurrencyServiceConfiguration() {
+    }
+
+    public CurrencyServiceConfiguration(String url, String username, String key) {
+        this.url = url;
+        this.username = username;
+        this.key = key;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
